bd58eab14b283ebf007db4cfec523987e8f687dd
nishi-hi <46593451+nishi-hi@users.noreply.github.com>
2021-08-01 12:02:01 +0900

    Update run.sh
    Specify container name.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -h example -p 80:80 \
+docker run -d --name example.com -h example -p 80:80 \
 --mount type=bind,source="$(pwd)/mount/etc-nginx-conf.d-example.conf",target="/etc/nginx/conf.d/example.conf" \
 --mount type=bind,source="$(pwd)/mount/srv-www-example",target="/srv/www/example" \
 --privileged example:latest /sbin/init
