f55d88a188e9e77e0218be77ec5e5b161443eee7
nishi-hi <46593451+nishi-hi@users.noreply.github.com>
2021-08-01 12:56:03 +0900

    Update index.php
    Add comments.

--- mount/srv-www-example/public/index.php
+++ mount/srv-www-example/public/index.php
@@ -1,5 +1,7 @@
 <?php
+  // If file or directory does not exist, all requests arrive this script.
   require_once('../script/controller.php');
+  // Controller reads request uri, then call suitable functions.
   $controller = new Controller();
   $controller->route();
 ?>
